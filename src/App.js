21069005588dc8745be65e3adfbe99209aba006c
Murad Sarker <sarker24@gmail.com>
2022-04-03 21:26:23 +0200

    Add routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Reviews from "./components/Home/Reviews/Reviews";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Blogs from "./components/Blogs/Blogs";
+import About from "./components/About/About";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Blog" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Blogs.css";
+
+const Blogs = () => {
+  return <div>blogs</div>;
+};
+
+export default Blogs;
