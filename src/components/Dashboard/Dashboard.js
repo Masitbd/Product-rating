4cd5b985d9b8eb2cdd101bc8daab559122aaca7a
Murad Sarker <sarker24@gmail.com>
2022-04-04 16:35:57 +0200

    Add first chart

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,42 @@
-import React from "react";
+import { data } from "autoprefixer";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import "./Dashboard.css";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [investments, setInvestments] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((response) => response.json())
+      .then((data) => setInvestments(data));
+  }, []);
+
+  return (
+    <LineChart
+      width={730}
+      height={250}
+      data={investments}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="month" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="month" stroke="#8884d8" />
+      <Line type="monotone" dataKey="investment" stroke="#82ca9d" />
+    </LineChart>
+  );
 };
 
 export default Dashboard;
