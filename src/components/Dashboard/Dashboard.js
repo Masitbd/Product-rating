6cdfc1a0682cdce91be8514936b1e8f6b38d6838
Murad Sarker <sarker24@gmail.com>
2022-04-05 02:17:38 +0200

    add dashboard chart

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,10 +2,16 @@ import { data } from "autoprefixer";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import {
+  Area,
+  AreaChart,
+  Bar,
+  BarChart,
   CartesianGrid,
   Legend,
   Line,
   LineChart,
+  Pie,
+  PieChart,
   Tooltip,
   XAxis,
   YAxis,
@@ -22,20 +28,114 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <LineChart
-      width={730}
-      height={250}
-      data={investments}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="month" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line type="monotone" dataKey="month" stroke="#8884d8" />
-      <Line type="monotone" dataKey="investment" stroke="#82ca9d" />
-    </LineChart>
+    <div className="grid grid-cols-1 gap-0 md:grid-cols-2 md:gap-2 mt-5">
+      <div>
+        <LineChart
+          width={730}
+          height={250}
+          data={investments}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="month" stroke="#8884d8" />
+          <Line type="monotone" dataKey="investment" stroke="#82ca9d" />
+        </LineChart>
+      </div>
+      <div>
+        <AreaChart
+          width={730}
+          height={250}
+          data={investments}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="month" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="investment"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+          <Area
+            type="monotone"
+            dataKey="revenue"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          />
+        </AreaChart>
+      </div>
+
+      <div className="mt-12">
+        <h1 className="text-2xl text-cyan-800 text-center">
+          Investment vs Revenue
+        </h1>
+        <BarChart
+          width={730}
+          height={300}
+          data={investments}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+          <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+        </BarChart>
+      </div>
+
+      <div className="mt-12">
+        <h1 className="text-2xl text-cyan-800 text-center">
+          Investment vs Revenue
+        </h1>
+        <PieChart width={730} height={250}>
+          <Pie
+            data={investments}
+            dataKey="investment"
+            nameKey="month"
+            cx="50%"
+            cy="50%"
+            outerRadius={50}
+            fill="#8884d8"
+          />
+          <Tooltip />
+          <Pie
+            data={investments}
+            dataKey="revenue"
+            cx="50%"
+            cy="50%"
+            innerRadius={60}
+            outerRadius={80}
+            fill="#82ca9d"
+            label
+          />
+        </PieChart>
+      </div>
+    </div>
   );
 };
 
